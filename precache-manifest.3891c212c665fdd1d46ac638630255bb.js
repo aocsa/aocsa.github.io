60cc250e638fa0310beac6165966eac7319cc46a
karla rejas <krejas@MacBook-Air-de-setup.local>
2019-03-28 13:58:49 -0500

    Updates

---
+++ precache-manifest.3891c212c665fdd1d46ac638630255bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/homepage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1d55219e7988c0454e32",
+    "url": "/homepage/static/js/runtime~main.336ecad5.js"
+  },
+  {
+    "revision": "266353186ad27797181e",
+    "url": "/homepage/static/js/main.d92c0bec.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/homepage/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "266353186ad27797181e",
+    "url": "/homepage/static/css/main.3da24e0e.chunk.css"
+  },
+  {
+    "revision": "0cebda109c969dab431604063b8422e5",
+    "url": "/homepage/index.html"
+  }
+];
