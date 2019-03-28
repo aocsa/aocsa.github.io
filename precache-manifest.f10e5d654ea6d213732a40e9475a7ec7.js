3a96123b000f732de8b4f8d11bca58a6cb7a57c6
Alexander <aocsa.cs@gmail.com>
2019-03-28 07:38:26 -0500

    [gh-pages] about aocsa

---
+++ precache-manifest.f10e5d654ea6d213732a40e9475a7ec7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c0e1e8a5d54c0e99a102",
+    "url": "/static/js/main.4a9e3853.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "c0e1e8a5d54c0e99a102",
+    "url": "/static/css/main.c2dde1e2.chunk.css"
+  },
+  {
+    "revision": "b7e66e65918839bc6615708f98b9bb68",
+    "url": "/index.html"
+  }
+];
